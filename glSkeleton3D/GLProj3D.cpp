c1ddf6c2f110d160889e151c50919fcf834308da
Argaide <marinabezares@gmail.com>
2014-03-14 11:29:52 +0100

    proyecto con gitignore

---
+++ glSkeleton3D/GLProj3D.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+USERES("GLProj3D.res");
+USEFORM("GLSkel3D.cpp", GLForm3D);
+USEUNIT("Cara.cpp");
+USEUNIT("Malla.cpp");
+USEUNIT("PV3D.cpp");
+USEUNIT("VerticeNormal.cpp");
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+        try
+        {
+                 Application->Initialize();
+                 Application->CreateForm(__classid(TGLForm3D), &GLForm3D);
+                 Application->Run();
+        }
+        catch (Exception &exception)
+        {
+                 Application->ShowException(&exception);
+        }
+        return 0;
+}
+//---------------------------------------------------------------------------
